0a3107258494d5810a07471aa4141ea9ccd72023
Arsene <komezarsene15@gmail.com>
2023-05-30 17:13:03 +0530

    Add Stateles Widget: Import external widges, extend classes, override build function with your own logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,6 @@
 import 'package:flutter/material.dart';
+import 'location_details.dart';
 
 void main() {
-  return runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Hello Flutter"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-                color: Colors.grey[50]
-            ),
-            child: Text("I am body of an app"),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                color: Colors.blueGrey
-            ),
-            child: Text("I am body of an app"),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                color: Colors.amberAccent
-            ),
-            child: Text("I am body of an app"),
-          ),
-        ],
-      )
-    ),
-  ));
+  return runApp(MaterialApp(home: LocationDetails()));
 }
